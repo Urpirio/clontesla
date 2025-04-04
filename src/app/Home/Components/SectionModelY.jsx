86e677ed738b29f4a38aaa951e51c28850245bc7
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-04-04 15:35:31 -0400

    Create SectionModelY.jsx

---
+++ src/app/Home/Components/SectionModelY.jsx
@@ -0,0 +1,24 @@
+import { ImgModelY } from "../../../../public/SectionModelY/Img";
+import { BtnModelY_LearMore, BtnModelY_OrderNow } from "../Btns/Btn_SectionModelY";
+
+export default function SectionModelY() {
+  return (
+    <section className="flex justify-center h-[100vh] items-start relative">
+      <article className="flex flex-col gap-5 justify-center items-center mt-20 z-20">
+        <div className="text-6xl text-white font-bold">
+          <h2>Model Y</h2>
+        </div>
+        <div className="flex flex-col text-center text-xl font-semibold text-white">
+          <span>Starting at $41,490</span>
+          <span>After $7,500 Federal Tax Credit</span>
+        </div>
+        <div className="flex gap-5">
+        <BtnModelY_OrderNow/>
+        <BtnModelY_LearMore/>
+        </div>
+      </article>
+      <ImgModelY/>
+     
+    </section>
+  )
+}

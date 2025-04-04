4c332bbf8676cc736ff1d94b9e852ad08b9c2d5d
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-04-04 15:35:19 -0400

    Se agrego un componente de los botones de la section ModelY

---
+++ src/app/Home/Btns/Btn_SectionModelY.js
@@ -0,0 +1,12 @@
+
+export const BtnModelY_OrderNow = () => {
+    return(
+        <button className="p-2 w-60 bg-[#3E6AE1] rounded-md text-white">Order Now</button> 
+    )
+};
+
+export const BtnModelY_LearMore = () => {
+    return(
+        <button className="p-2 w-60 bg-white rounded-md text-gray-900">Learn More</button>
+    )
+};

30f8ea3be1b445c5acac11096fee9f6bc9525b8b
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-04-04 15:35:52 -0400

    Se agrego un componente Body

---
+++ src/app/page.jsx
@@ -0,0 +1,11 @@
+import Header from "./Home/Components/Header";
+import SectionModelY from "./Home/Components/SectionModelY";
+
+export default function Home() {
+  return (
+    <body>
+      <Header/>
+      <SectionModelY/>
+    </body>
+  )
+}

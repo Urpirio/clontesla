90613b9d143265c7536d115281883da0c9f77e22
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-04-04 15:34:55 -0400

    Se agrego un componentes de los botones del header

---
+++ src/app/Home/Btns/Btn_Header.js
@@ -0,0 +1,13 @@
+import { IconGlobal, IconPerfil, IconQuestion } from "../../../../public/Header/Img";
+
+export const BtnSopport = () => {
+    return(<button className=" h-8 w-8"><IconQuestion/></button>)
+};
+
+export const BtnGlobal = () => {
+    return(<button className=" h-8 w-8"><IconGlobal/></button>)
+};
+
+export const BtnPerfil = () => {
+    return(<button className=" h-8 w-8"><IconPerfil/></button>)
+};

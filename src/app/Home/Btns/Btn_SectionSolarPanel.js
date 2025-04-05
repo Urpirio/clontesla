c60bb343aa2f6cbdeaa768fd17a8db74e9e40c61
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-04-05 10:33:50 -0400

    Se agrego los componentes de botones de la section Solar Panel

---
+++ src/app/Home/Btns/Btn_SectionSolarPanel.js
@@ -0,0 +1,11 @@
+export const BtnSolarPanel_OrderNow = () => {
+    return(
+        <button className="p-2 lg:w-60 w-40 bg-[#3E6AE1] rounded-md text-white font-bold">Order Now</button> 
+    )
+};
+
+export const BtnSolarPanel_LearMore = () => {
+    return(
+        <button className="p-2 lg:w-60 w-40 bg-white rounded-md text-gray-900 font-bold">Learn More</button>
+    )
+};

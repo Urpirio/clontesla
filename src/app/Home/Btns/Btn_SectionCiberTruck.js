3ae8b81b1469285ca8921e1ddd6c1def798cb5c9
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-04-05 10:32:32 -0400

    Se agrego los componentes de botones de la section del Ciber truck

---
+++ src/app/Home/Btns/Btn_SectionCiberTruck.js
@@ -0,0 +1,12 @@
+
+export const BtnModelCiber_OrderNow = () => {
+    return(
+        <button className="p-2 lg:w-60 w-40 bg-black opacity-70 rounded-xs text-white font-bold hover:opacity-100 hover:bg-white hover:text-black">Order Now</button> 
+    )
+};
+
+export const BtnModelCiber_LearMore = () => {
+    return(
+        <button className="p-2 lg:w-60 w-40 bg-black opacity-70 rounded-xs text-white font-bold hover:opacity-100 hover:bg-white hover:text-black">Learn More</button>
+    )
+};

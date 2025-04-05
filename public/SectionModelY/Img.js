ef0cf688cad8919ce6b5189ffba6e86708bcbfc8
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-04-05 10:31:06 -0400

    Se hizo un pequeña modificacion

--- public/SectionModelY/Img.js
+++ public/SectionModelY/Img.js
@@ -1,8 +1,8 @@
 
 export const ImgModelY = () => {
     return(
-        <picture className="z-10 flex absolute justify-center items-center scroll-none h-screen overflow-hidden ">
-            <img className="fixed h-[100%] min-w-[1320px] lg:w-[100vw]"    src="https://digitalassets.tesla.com/tesla-contents/image/upload/f_auto,q_auto/Homepage-Model-Y-2-Hero-Desktop-APAC-LHD.png" alt="Model Y" />
+        <picture className="z-10 flex absolute justify-center items-center  h-screen overflow-hidden ">
+            <img className=" h-[100%] min-w-[1320px] lg:w-[100vw] object-cover"    src="https://digitalassets.tesla.com/tesla-contents/image/upload/f_auto,q_auto/Homepage-Model-Y-2-Hero-Desktop-APAC-LHD.png" alt="Model Y" />
         </picture>
     )
 }

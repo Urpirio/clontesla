fe652a1b53cea113700b7d602e9eabd5a7a895f6
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-04-05 10:32:58 -0400

    Se agrego los componentes de botones de la section Model S

---
+++ src/app/Home/Btns/Btn_SectionModelS.js
@@ -0,0 +1,11 @@
+export const BtnModelS_OrderNow = () => {
+    return(
+        <button className="p-2 lg:w-60 w-40 bg-[#3E6AE1] rounded-md text-white font-bold">Order Now</button> 
+    )
+};
+
+export const BtnModelS_LearMore = () => {
+    return(
+        <button className="p-2 lg:w-60 w-40 bg-white rounded-md text-gray-900 font-bold">Learn More</button>
+    )
+};

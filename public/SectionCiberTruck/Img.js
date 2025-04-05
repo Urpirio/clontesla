a02f0f14d581c23521183fbc8fa57b12e02f3f16
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-04-05 10:29:58 -0400

    Imgenes de la seccion de la ciber truck

---
+++ public/SectionCiberTruck/Img.js
@@ -0,0 +1,16 @@
+
+export const ImgLogoCiber = () => {
+    return(
+        <picture>
+            <img src="https://upload.wikimedia.org/wikipedia/commons/6/69/Cybertrucklogo.svg" alt="Logo Ciber truck"/>
+        </picture>
+    )
+};
+
+export const ImgCiber = () => {
+    return(
+        <picture className="z-10 flex absolute justify-center items-center h-[100%] overflow-hidden">
+            <img className="h-[100%] min-w-[1320px] lg:w-[100vw] object-cover"   src="https://digitalassets.tesla.com/tesla-contents/image/upload/f_auto,q_auto/Homepage-Cybertruck-Desktop-v3.png" alt="Model C"/>
+        </picture>
+    )
+};

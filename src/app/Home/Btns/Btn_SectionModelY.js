d2e9d77df763b4963b2f05ade7c89128a47bad11
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-04-05 10:33:32 -0400

    Se hicieron pequeñas mejoras

--- src/app/Home/Btns/Btn_SectionModelY.js
+++ src/app/Home/Btns/Btn_SectionModelY.js
@@ -1,12 +1,12 @@
 
 export const BtnModelY_OrderNow = () => {
     return(
-        <button className="p-2 w-60 bg-[#3E6AE1] rounded-md text-white">Order Now</button> 
+        <button className="p-2 lg:w-60 w-40 bg-[#3E6AE1] rounded-md text-white font-bold">Order Now</button> 
     )
 };
 
 export const BtnModelY_LearMore = () => {
     return(
-        <button className="p-2 w-60 bg-white rounded-md text-gray-900">Learn More</button>
+        <button className="p-2 lg:w-60 w-40 bg-white rounded-md text-gray-900 font-bold">Learn More</button>
     )
 };

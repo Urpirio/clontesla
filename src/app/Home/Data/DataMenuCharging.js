32434f9f36cedce437e4af8d4e9aca390926f48b
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-04-06 22:22:03 -0400

    Se agrego la informacion del menu de Charging

---
+++ src/app/Home/Data/DataMenuCharging.js
@@ -0,0 +1,31 @@
+export const DataMenuCharging = [
+    {
+        title: `Help Me Charge`,
+        Link: ``,
+    },
+    {
+        title: `Charging Calculator`,
+        Link: ``,
+    },
+    {
+        title: `Charging With NACS`,
+        Link: ``,
+    },
+    {
+        title: `Supercharger Voting`,
+        Link: ``,
+    },
+    {
+        title: `Host a Supercharger`,
+        Link: ``,
+    },
+    {
+        title: `Commercial Charging`,
+        Link: ``,
+    },
+    {
+        title: `Host Wall Connectors`,
+        Link: ``,
+    },
+    
+];

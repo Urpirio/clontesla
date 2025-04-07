82667c07bc2d41cfa4d0e29d99f7bed4e99ddfa8
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-04-06 22:22:50 -0400

    se agregaron los menus despleagables

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,11 +1,15 @@
 import Header from "./Home/Components/Header";
 import GlobalSections from "./Home/Components/GlobalSections";
 import SectionModelY from "./Home/Components/SectionModelY";
+import {ElementsMenuCharging, ElementsMenuEnergy, ElementsMenuVehicles }from "./Home/Components/ElementsMenu";
 
 export default function Home() {
   return (
     <body>
       <Header/>
+      <ElementsMenuVehicles/>
+      <ElementsMenuEnergy/>
+      <ElementsMenuCharging/>
       <SectionModelY/>
       <GlobalSections/>
     </body>

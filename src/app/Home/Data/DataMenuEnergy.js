00a8f853ce03bd4ff58325881be011077dd0a5f9
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-04-06 22:22:31 -0400

    Se agrego la data del menu desplegable de energia

---
+++ src/app/Home/Data/DataMenuEnergy.js
@@ -0,0 +1,30 @@
+export const DataMenuEnergy = [
+    {
+        title: `Schedule a Consultation`,
+        Link: ``,
+    },
+    {
+        title: `Why Solar`,
+        Link: ``,
+    },
+    {
+        title: `Incentives`,
+        Link: ``,
+    },
+    {
+        title: `Support`,
+        Link: ``,
+    },
+    {
+        title: `Partner with Tesla`,
+        Link: ``,
+    },
+    {
+        title: `Commercial`,
+        Link: ``,
+    },
+    {
+        title: `Utilities`,
+        Link: ``,
+    },
+]

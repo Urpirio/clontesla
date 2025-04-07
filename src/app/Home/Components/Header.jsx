bb00a5481411e4cbd90f02d4431cc86eacd2bdf6
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-04-06 22:20:32 -0400

    Se hicieron mejoras en el Header

--- src/app/Home/Components/Header.jsx
+++ src/app/Home/Components/Header.jsx
@@ -4,12 +4,20 @@ import { BtnGlobal, BtnPerfil, BtnSopport } from "../Btns/Btn_Header";
 export default function Header() {
     
   return (
-    <header className="flex items-center justify-between fixed w-[100%] p-3 z-50">
-        <div className="flex flex-grow basis-0 justify-star text-white  h-3">
+    <header className="flex items-center justify-between fixed w-[100%] p-3 z-50 transition-all  duration-300 hover:bg-white text-white hover:text-black ">
+        <div className="flex flex-grow basis-0 justify-star   h-3">
             <LogoTesla/>
         </div>
         <nav className="lg:flex hidden justify-center items-center text-[16px] font-semibold ">
-            <ul className="flex justify-center items-center text-white gap-5">
+            <ul className="flex justify-center items-center
+            [&ul>li]:hover:bg-gray-200
+              [&ul>li]:py-1
+              [&ul>li]:px-3
+              [&ul>li]:rounded-[5px]
+              [&ul>li]:transition-all
+              [&ul>li]:duration-300
+              [&ul>li]:cursor-pointer
+            ">
                 <li>Vehicles</li>
                 <li>Energy</li>
                 <li>Charging</li>
@@ -17,7 +25,7 @@ export default function Header() {
                 <li>Shop</li>
             </ul>
         </nav>
-        <div className="lg:flex hidden text-white justify-end gap-2 flex-grow basis-0">
+        <div className="lg:flex hidden  justify-end gap-2 flex-grow basis-0">
             <BtnSopport/>
             <BtnGlobal/>
             <BtnPerfil/>

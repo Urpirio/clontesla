d6454e0875749c30af461dcd4ae9e989d30fd71c
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-04-06 22:21:34 -0400

    Se agrego el array de datos de el menu de vehicles

---
+++ src/app/Home/Data/DataMenuCars.js
@@ -0,0 +1,38 @@
+export const DataMenuCars = [
+    {
+        title: `Help Me Choose`,
+        Link: ``,
+    },
+    {
+        title: `Demo Drive`,
+        Link: ``,
+    },
+    {
+        title: `Trade-in`,
+        Link: ``,
+    },
+    {
+        title: `Compare`,
+        Link: ``,
+    },
+    {
+        title: `Fleet`,
+        Link: ``,
+    },
+    {
+        title: `Semi`,
+        Link: ``,
+    },
+    {
+        title: `Roadster`,
+        Link: ``,
+    },
+    {
+        title: `Federal Tax Credit`,
+        Link: ``,
+    },
+    {
+        title: `We, Robot`,
+        Link: ``,
+    },
+]

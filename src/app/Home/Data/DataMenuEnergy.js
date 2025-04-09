e1b8b1f96a8eb5f264bdef3b8c218727c3691428
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-04-08 21:24:31 -0400

    Se agrego un ID para el key que identifica la clave

--- src/app/Home/Data/DataMenuEnergy.js
+++ src/app/Home/Data/DataMenuEnergy.js
@@ -1,29 +1,36 @@
 export const DataMenuEnergy = [
     {
+        id: 1,
         title: `Schedule a Consultation`,
         Link: ``,
     },
     {
+        id: 2,
         title: `Why Solar`,
         Link: ``,
     },
     {
+        id: 3,
         title: `Incentives`,
         Link: ``,
     },
     {
+        id: 4,
         title: `Support`,
         Link: ``,
     },
     {
+        id: 5,
         title: `Partner with Tesla`,
         Link: ``,
     },
     {
+        id: 6,
         title: `Commercial`,
         Link: ``,
     },
     {
+        id: 7,
         title: `Utilities`,
         Link: ``,
     },

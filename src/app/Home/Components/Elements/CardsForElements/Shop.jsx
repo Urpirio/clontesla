35c53eafa86f02b121a6d3d5caf704df9843542e
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-04-08 21:22:42 -0400

    Se modifico el llamado de la api y ahora se hace desde el cliente

--- src/app/Home/Components/Elements/CardsForElements/Shop.jsx
+++ src/app/Home/Components/Elements/CardsForElements/Shop.jsx
@@ -1,13 +1,34 @@
+'use client';
+import { useState } from "react";
+import { useEffect } from "react";
 
-export async function Shop() {
-    const res = await fetch('https://randomapi.com/api/afa7c82c45b09c35c94b81a2c02561cb');
-    const Data = await res.json();
+export function Shop() {
+
+    const [Data, setData] = useState([]);
+    const [HaveData, setHaveData] = useState(false);
+
+    useEffect(()=>{
+        fetch('https://randomapi.com/api/afa7c82c45b09c35c94b81a2c02561cb')
+        .then( res => res.json())
+        .then((Data)=>{
+            setData(Data);
+            setHaveData(true);
+        }).catch((err)=>{
+            console.error(err)
+        });
+    });
+
+    if(HaveData === false){
+        return(
+            <span>Cargando...</span>
+        )
+    }
 
     return (
       <div className="flex justify-center items-center flex-wrap w-[60%]">
         {Data.results[0].Shop.map( DT => {
             return(
-                <article className="p-2">
+                <article key={DT.id} className="p-2">
                     <div className="flex flex-col items-center">
                         <img className=" w-50 object-contain" src={DT.Img} alt={DT.Modelo} />
                         <h2 className="text-md font-bold">{DT.Modelo}</h2>

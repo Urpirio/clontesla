a97666c58032274e72a2fe6f29b9b0aabf1932d2
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-04-08 21:22:56 -0400

    Se agrego un Key ID

--- src/app/Home/Components/Elements/Menu/ChargingMenu.jsx
+++ src/app/Home/Components/Elements/Menu/ChargingMenu.jsx
@@ -4,10 +4,10 @@ import { DataMenuCharging } from "@/app/Home/Data/DataMenuCharging";
 export default function ChargingMenu() {
 
   return (
-    <menu className="flex flex-col w-[20%]">
+    <menu className="flex gap-2 flex-col w-[20%]">
         {DataMenuCharging.map(DMC => {
             return(
-                <Link className="pl-9" href={DMC.Link}>{DMC.title}</Link>
+                <Link key={DMC.id} className="font-semibold text-[14px] pl-9 hover:underline" href={DMC.Link}>{DMC.title}</Link>
             )
         })}
     </menu>

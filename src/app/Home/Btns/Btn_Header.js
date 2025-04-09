936b5b35e92c2b4f0a830f4e1a9bb89b5b267a39
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-04-08 21:21:12 -0400

    Se le agrego un clase los botones de header

--- src/app/Home/Btns/Btn_Header.js
+++ src/app/Home/Btns/Btn_Header.js
@@ -1,13 +1,13 @@
 import { IconGlobal, IconPerfil, IconQuestion } from "../../../../public/Header/Img";
 
 export const BtnSopport = () => {
-    return(<button className=" h-8 w-8"><IconQuestion/></button>)
+    return(<button className=" h-8 w-8 HeaderBtn1"><IconQuestion/></button>)
 };
 
 export const BtnGlobal = () => {
-    return(<button className=" h-8 w-8"><IconGlobal/></button>)
+    return(<button className=" h-8 w-8 HeaderBtn2"><IconGlobal/></button>)
 };
 
 export const BtnPerfil = () => {
-    return(<button className=" h-8 w-8"><IconPerfil/></button>)
+    return(<button className=" h-8 w-8 HeaderBtn3"><IconPerfil/></button>)
 };

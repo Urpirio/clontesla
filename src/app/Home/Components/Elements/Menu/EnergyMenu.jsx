91cca10d8a4311fea58d4a5714c2aebaf336b374
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-04-08 21:23:17 -0400

    Se agrego un key ID y nuevos estilos

--- src/app/Home/Components/Elements/Menu/EnergyMenu.jsx
+++ src/app/Home/Components/Elements/Menu/EnergyMenu.jsx
@@ -3,9 +3,9 @@ import { DataMenuEnergy } from "@/app/Home/Data/DataMenuEnergy";
 
 export default function EnergyMenu() {
   return (
-    <menu className="flex flex-col w-[20%]" >
+    <menu className="flex gap-2 flex-col w-[20%]" >
         {DataMenuEnergy.map(DME => {
-            return(<Link className="pl-9" href={DME.Link}>{DME.title}</Link>)
+            return(<Link key={DME.id} className="font-semibold text-[14px] pl-9 hover:underline" href={DME.Link}>{DME.title}</Link>)
         })}
     </menu>
   )

dfba6db972733439425a3e4f85e67be601116bd4
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-04-09 08:01:21 -0400

    Se agrego un condicional

--- src/app/Home/func/HoverHeader.js
+++ src/app/Home/func/HoverHeader.js
@@ -1,3 +1,4 @@
+import { IsMenuDeploy } from "./DeployMenu";
 
 export const HoverHeader_MouseEnter = () =>{
     for(let x = 1; x < 5; x++){
@@ -14,15 +15,19 @@ export const HoverHeader_MouseEnter = () =>{
 };
 
 export const HoverHeader_MouseLeave = () => {
-    for(let x = 1; x < 5; x++){
-        document.getElementById('LogoTesla').style.color = 'white';
-    };
-
-    for(let x = 1; x < 5; x++){
-        document.querySelector(`.Option${x}`).style.color = 'white';
-    };
-
-    for(let x = 1 ; x < 4; x++){
-        document.querySelector(`.HeaderBtn${x}`).style.color = 'white';
+    if(IsMenuDeploy.Boleano === true){
+        
+    }else{
+        for(let x = 1; x < 5; x++){
+            document.getElementById('LogoTesla').style.color = 'white';
+        };
+    
+        for(let x = 1; x < 5; x++){
+            document.querySelector(`.Option${x}`).style.color = 'white';
+        };
+    
+        for(let x = 1 ; x < 4; x++){
+            document.querySelector(`.HeaderBtn${x}`).style.color = 'white';
+        };
     }
 }

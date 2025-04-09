e6158cef52da6f8c3341e295854330b504837d66
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-04-08 21:22:16 -0400

    Se modifico el llamado de la api y ahora se hace desde el cliente

--- src/app/Home/Components/Elements/CardsForElements/Energy.jsx
+++ src/app/Home/Components/Elements/CardsForElements/Energy.jsx
@@ -1,15 +1,36 @@
+'use client';
 import Link from "next/link";
+import { useState } from "react";
+import { useEffect } from "react";
 
-export async function Energy() {
-    const res = await fetch('https://randomapi.com/api/afa7c82c45b09c35c94b81a2c02561cb');
-    const Data = await res.json();
+export function Energy() {
+
+    const [Data, setData] = useState([]);
+    const [HaveData, setHaveData] = useState(false)
+
+    useEffect(()=>{
+        fetch('https://randomapi.com/api/afa7c82c45b09c35c94b81a2c02561cb')
+        .then(res => res.json())
+        .then((Data)=>{
+            setData(Data);
+            setHaveData(true);
+        }).catch((err)=>{
+            console.error(err);
+        })
+    });
+
+    if(HaveData === false){
+        return(
+            <span>Cargando...</span>
+        )
+    };
     
 
   return (
     <div className="flex justify-start items-center flex-wrap border-r-2 border-gray-300 w-[60%]">
         {Data.results[0].Energy.map( E => {
             return(
-                <article className="p-2">
+                <article key={E.id} className="p-2">
                     <div className="flex flex-col items-center">
                         <img className=" w-50 object-contain" src={E.Img} alt={E.Modelo} />
                         <h2 className="text-md font-bold">{E.Modelo}</h2>
@@ -22,8 +43,7 @@ export async function Energy() {
                     </div>
                 </article>
             )
-        })
-        }
+        })}
     </div>
   )
 }

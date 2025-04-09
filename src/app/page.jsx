2ab9814d5d9972e06e4546dcd18717d5828eaeb5
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-04-08 21:26:02 -0400

    Se le agrego una clase de posicion relativa al Body

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,11 +1,17 @@
+'use client';
 import Header from "./Home/Components/Sections/Header";
 import GlobalSections from "./Home/Components/Sections/GlobalSections";
 import SectionModelY from "./Home/Components/Sections/SectionModelY";
 import {ElementsMenuCharging, ElementsMenuEnergy, ElementsMenuShop, ElementsMenuVehicles }from "./Home/Components/Elements/BodyMenu/ElementsMenu";
+import { Scrolling } from "./Home/func/Scrolling";
+// import { useEffect } from "react";
 
 export default function Home() {
+  // useEffect(()=>{
+  //   Scrolling();
+  // });
   return (
-    <body>
+    <body  className="relative">
       <Header/>
       <ElementsMenuVehicles/>
       <ElementsMenuEnergy/>

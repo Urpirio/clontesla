e6221f4008a7ed63ab7feec564ab48c635a5f72b
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-04-08 21:24:05 -0400

    Se agrego un ide para el

--- src/app/Home/Data/DataMenuCars.js
+++ src/app/Home/Data/DataMenuCars.js
@@ -1,37 +1,46 @@
 export const DataMenuCars = [
     {
+        id: 1,
         title: `Help Me Choose`,
         Link: ``,
     },
     {
+        id: 2,
         title: `Demo Drive`,
         Link: ``,
     },
     {
+        id: 3,
         title: `Trade-in`,
         Link: ``,
     },
     {
+        id: 4,
         title: `Compare`,
         Link: ``,
     },
     {
+        id: 5,
         title: `Fleet`,
         Link: ``,
     },
     {
+        id: 6,
         title: `Semi`,
         Link: ``,
     },
     {
+        id: 7,
         title: `Roadster`,
         Link: ``,
     },
     {
+        id: 8,
         title: `Federal Tax Credit`,
         Link: ``,
     },
     {
+        id: 9,
         title: `We, Robot`,
         Link: ``,
     },

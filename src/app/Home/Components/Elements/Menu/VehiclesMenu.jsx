a46e9f1409f5b6393b1713c326ab70b74ce76a0c
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-04-08 21:23:21 -0400

    Se agrego un key ID y nuevos estilos

--- src/app/Home/Components/Elements/Menu/VehiclesMenu.jsx
+++ src/app/Home/Components/Elements/Menu/VehiclesMenu.jsx
@@ -5,9 +5,9 @@ import { DataMenuCars } from "@/app/Home/Data/DataMenuCars";
 
 export default function VehiclesMenu() {
    return(
-    <menu className="flex flex-col w-[20%]" >
+    <menu className="flex gap-2 flex-col w-[20%]" >
        {DataMenuCars.map( DMC => {
-        return(<Link className="pl-9" href={DMC.Link}>{DMC.title}</Link>)
+        return(<Link key={DMC.id} className="font-semibold text-[15px] pl-9 hover:underline" href={DMC.Link}>{DMC.title}</Link>)
        })} 
     </menu>
    )

86ef756738fca0b4c6de958ec3a5c717001ff663
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-04-08 21:21:54 -0400

    Se modifico el llamado de la api y ahora se hace desde el cliente

--- src/app/Home/Components/Elements/CardsForElements/Charging.jsx
+++ src/app/Home/Components/Elements/CardsForElements/Charging.jsx
@@ -1,14 +1,35 @@
+'use client';
 import Link from "next/link";
+import { useState } from "react";
+import { useEffect } from "react";
 
-export const  Charging = async () => {
-    const res = await fetch('https://randomapi.com/api/afa7c82c45b09c35c94b81a2c02561cb');
-    const Data = await res.json();
+export const Charging = () => {
+    const [Data, setData] = useState([]);
+    const [HaveData,setHaveData] = useState(false);
+
+    useEffect(()=>{
+        fetch('https://randomapi.com/api/afa7c82c45b09c35c94b81a2c02561cb')
+        .then( res => res.json())
+        .then((Data)=>{
+            setData(Data)
+            setHaveData(true)
+        }).catch((err)=>{
+            console.log(err)
+        })
+    });
+
+    if(HaveData === false){
+        return(
+            <span>Cargando...</span>
+        )
+    }
+
 
   return (
     <div className="flex justify-end flex-wrap border-r-2 py-5 border-gray-300 w-[50%]">
         {Data.results[0].Charging.map( Cg => {
             return(
-                <article className="p-2">
+                <article key={Cg.id} className="p-2">
                     <div className="flex flex-col items-center">
                         <img className=" w-50 object-contain" src={Cg.Img} alt={Cg.Modelo} />
                         <h2 className="text-md font-bold">{Cg.Modelo}</h2>

1e77e3b37a767e037027a7a3f0f55f69a9c7a772
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-04-08 21:24:28 -0400

    Se agrego un ID para el key que identifica la clave

--- src/app/Home/Data/DataMenuCharging.js
+++ src/app/Home/Data/DataMenuCharging.js
@@ -1,29 +1,36 @@
 export const DataMenuCharging = [
     {
+        id: 1,
         title: `Help Me Charge`,
         Link: ``,
     },
     {
+        id: 2,
         title: `Charging Calculator`,
         Link: ``,
     },
     {
+        id: 3,
         title: `Charging With NACS`,
         Link: ``,
     },
     {
+        id: 4,
         title: `Supercharger Voting`,
         Link: ``,
     },
     {
+        id: 5,
         title: `Host a Supercharger`,
         Link: ``,
     },
     {
+        id: 6,
         title: `Commercial Charging`,
         Link: ``,
     },
     {
+        id: 7,
         title: `Host Wall Connectors`,
         Link: ``,
     },
